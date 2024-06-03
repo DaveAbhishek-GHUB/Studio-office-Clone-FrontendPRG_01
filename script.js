83abc07c9972752fe6cc541e62abac764f1ea8ee
Dave Abhishek <abhishekjdave0309@gmail.com>
2024-06-03 17:28:27 +0530

    1 to 4th page animation completed

--- script.js
+++ script.js
@@ -1,4 +1,70 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
-});
+
+gsap.ticker.fps(60);
+
+// Site-name Animations
+
+gsap.from(".nav-bar-r", {
+    opacity:0
+})
+
+gsap.from(".web-name-info img", {
+    y:"100%"
+})
+
+gsap.from(".web-name-info-bottom h6", {
+    y:"100%"
+}) 
+
+// photo-Grid Animations
+gsap.from(".grid-block-wrapper-02", {
+    y:"20%",
+    scrollTrigger:".grid-block-wrapper-02"
+})
+
+gsap.from(".grid-block-wrapper-03", {
+    y:"20%",
+    scrollTrigger:".grid-block-wrapper-03",
+})
+
+gsap.from(".grid-block-wrapper-04", {
+    scale:0,
+    scrollTrigger:".grid-block-wrapper-03",
+
+})
+
+gsap.from(".grid-block-wrapper-06", {
+    scale:0,
+    scrollTrigger:".grid-block-wrapper-06",
+})
+
+gsap.from(".grid-block-wrapper-07", {
+    y:"20%",
+    scrollTrigger:".grid-block-wrapper-07",
+})
+
+gsap.from(".grid-block-wrapper-08", {
+    scale:0,
+    scrollTrigger:".grid-block-wrapper-08",
+})
+
+gsap.from(".fst-image", {
+    y:"20%",
+    scrollTrigger:".fst-image",
+})
+
+// WHO section animations
+
+gsap.from(".rainbow-image img", {
+    scale:0,
+    scrollTrigger:".rainbow-image"
+})
+
+gsap.from(".sun-image img", {
+    scale:0,
+    scrollTrigger:".rainbow-image"
+})
+
+gsap.from(".star-image img", {
+    scale:0,
+    scrollTrigger:".rainbow-image"
+})
